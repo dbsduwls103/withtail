baec3bcceead31073422248cdb5f53f632147fe8
hanjeongsu <hanjeongsu@DESKTOP-24MFFFJ>
2023-06-19 20:42:15 +0900

    member 추가

---
+++ withtail/src/main/java/com/sp/withtail/member/MemberServiceImpl.java
@@ -0,0 +1,152 @@
+package com.sp.withtail.member;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sp.withtail.common.dao.CommonDAO;
+
+
+@Service("member.memberService")
+public class MemberServiceImpl implements MemberService {
+	@Autowired
+	private CommonDAO dao;
+	
+	@Override
+	public Member loginMember(String userId) {
+		Member dto = null;
+
+		try {
+			dto = dao.selectOne("member.loginMember", userId);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return dto;
+	}
+
+	@Override
+	public void insertMember(Member dto) throws Exception {
+		try {
+			if (dto.getEmail1().length() != 0 && dto.getEmail2().length() != 0) {
+				dto.setEmail(dto.getEmail1() + "@" + dto.getEmail2());
+			}
+
+
+			long memberSeq = dao.selectOne("member.memberSeq");
+			dto.setMemberIdx(memberSeq);
+
+			// 회원정보 저장
+			dao.insertData("member.insertMember", memberSeq);
+
+			// dao.insertData("member.insertMember1", dto);
+			// dao.insertData("member.insertMember2", dto);
+			dao.updateData("member.insertMember12", dto); // member1, member2 테이블 동시에
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	public Member readMember(String userId) {
+		Member dto = null;
+
+		try {
+			dto = dao.selectOne("member.readMember", userId);
+
+			if (dto != null) {
+				if (dto.getEmail() != null) {
+					String[] s = dto.getEmail().split("@");
+					dto.setEmail1(s[0]);
+					dto.setEmail2(s[1]);
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return dto;
+	}
+
+	@Override
+	public Member readMember(long memberIdx) {
+		Member dto = null;
+
+		try {
+			dto = dao.selectOne("member.readMember2", memberIdx);
+
+			if (dto != null) {
+				if (dto.getEmail() != null) {
+					String[] s = dto.getEmail().split("@");
+					dto.setEmail1(s[0]);
+					dto.setEmail2(s[1]);
+				}
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return dto;
+	}
+
+	@Override
+	public void updateMembership(Map<String, Object> map) throws Exception {
+		try {
+			dao.updateData("member.updateMembership", map);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	public void updateLastLogin(String userId) throws Exception {
+		try {
+			dao.updateData("member.updateLastLogin", userId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	public void updateMember(Member dto) throws Exception {
+		try {
+			if (dto.getEmail1().length() != 0 && dto.getEmail2().length() != 0) {
+				dto.setEmail(dto.getEmail1() + "@" + dto.getEmail2());
+			}
+
+
+			dao.updateData("member.updateMember1", dto);
+			dao.updateData("member.updateMember2", dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	public void deleteMember(Map<String, Object> map) throws Exception {
+		try {
+			map.put("membership", 0);
+			updateMembership(map);
+
+			dao.deleteData("member.deleteMember2", map);
+			dao.deleteData("member.deleteMember1", map);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	public void generatePwd(Member dto) throws Exception {
+		// 10 자리 임시 패스워드 생성
+
+	}
+}
