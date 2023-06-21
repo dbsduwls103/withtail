6ed52f90b424ca8be740785af0d7537654d1d107
dbsdu <dbsdu@yyj>
2023-06-21 16:19:04 +0900

    여진 - Shop(DTO) 추가

---
+++ withtail/src/main/java/com/sp/withtail/shop/Shop.java
@@ -0,0 +1,194 @@
+package com.sp.withtail.shop;
+
+public class Shop {
+	private String ctNum;
+	private String ctName;
+	
+	private long prodNum;
+	private String prodName;
+	private int discount;
+	private String prodContent;
+	private String regDate;
+	private String updDate;
+	private String madeby;
+	private int deliveryFee;
+	private int price;
+	private int showNotice;
+	
+	private String manufactDate;
+	private String expriy;
+	private String country;
+	
+	private long opNum1;
+	private String opName;
+	private long opNum2;
+	private String opContent;
+	private int stock;
+	private int extraPrice;
+	private int coupon;
+	
+	private long photoNum;
+	private long photoName;
+	
+	private String userId;
+	private String userName;
+	
+	public String getCtNum() {
+		return ctNum;
+	}
+	public void setCtNum(String ctNum) {
+		this.ctNum = ctNum;
+	}
+	public String getCtName() {
+		return ctName;
+	}
+	public void setCtName(String ctName) {
+		this.ctName = ctName;
+	}
+	public long getProdNum() {
+		return prodNum;
+	}
+	public void setProdNum(long prodNum) {
+		this.prodNum = prodNum;
+	}
+	public String getProdName() {
+		return prodName;
+	}
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	public int getDiscount() {
+		return discount;
+	}
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+	public String getProdContent() {
+		return prodContent;
+	}
+	public void setProdContent(String prodContent) {
+		this.prodContent = prodContent;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public String getUpdDate() {
+		return updDate;
+	}
+	public void setUpdDate(String updDate) {
+		this.updDate = updDate;
+	}
+	public String getMadeby() {
+		return madeby;
+	}
+	public void setMadeby(String madeby) {
+		this.madeby = madeby;
+	}
+	public int getDeliveryFee() {
+		return deliveryFee;
+	}
+	public void setDeliveryFee(int deliveryFee) {
+		this.deliveryFee = deliveryFee;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getShowNotice() {
+		return showNotice;
+	}
+	public void setShowNotice(int showNotice) {
+		this.showNotice = showNotice;
+	}
+	public String getManufactDate() {
+		return manufactDate;
+	}
+	public void setManufactDate(String manufactDate) {
+		this.manufactDate = manufactDate;
+	}
+	public String getExpriy() {
+		return expriy;
+	}
+	public void setExpriy(String expriy) {
+		this.expriy = expriy;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public long getOpNum1() {
+		return opNum1;
+	}
+	public void setOpNum1(long opNum1) {
+		this.opNum1 = opNum1;
+	}
+	public String getOpName() {
+		return opName;
+	}
+	public void setOpName(String opName) {
+		this.opName = opName;
+	}
+	public long getOpNum2() {
+		return opNum2;
+	}
+	public void setOpNum2(long opNum2) {
+		this.opNum2 = opNum2;
+	}
+	public String getOpContent() {
+		return opContent;
+	}
+	public void setOpContent(String opContent) {
+		this.opContent = opContent;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public int getExtraPrice() {
+		return extraPrice;
+	}
+	public void setExtraPrice(int extraPrice) {
+		this.extraPrice = extraPrice;
+	}
+	public int getCoupon() {
+		return coupon;
+	}
+	public void setCoupon(int coupon) {
+		this.coupon = coupon;
+	}
+	public long getPhotoNum() {
+		return photoNum;
+	}
+	public void setPhotoNum(long photoNum) {
+		this.photoNum = photoNum;
+	}
+	public long getPhotoName() {
+		return photoName;
+	}
+	public void setPhotoName(long photoName) {
+		this.photoName = photoName;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+}
