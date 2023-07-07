265f79cbc925509cadc0e7ae5c5230b401accfbf
dbsdu <dbsdu@yyj>
2023-07-07 11:44:08 +0900

    여진 - OrderController 수정

--- withtail/src/main/java/com/sp/withtail/order/OrderController.java
+++ withtail/src/main/java/com/sp/withtail/order/OrderController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -132,6 +133,12 @@ public class OrderController {
     	model.put("state", state);
     	
     	return model;
+	}
+	
+	@GetMapping("complete")
+	public String complete() {
+		
+		return ".order.complete";
 	}
 	
 }
