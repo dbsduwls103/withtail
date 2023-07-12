467e36ca5593d33cc16396a6fd02773e825025e8
dbsdu <dbsdu@yyj>
2023-07-12 11:07:08 +0900

    여진 - 신상 페이징 수정

--- withtail/src/main/java/com/sp/withtail/common/MyUtilCustom.java
+++ withtail/src/main/java/com/sp/withtail/common/MyUtilCustom.java
@@ -31,11 +31,19 @@ public class MyUtilCustom extends MyUtil {
 
 		sb.append("<ul class='d-flex gap-1 justify-content-center'>");
 		
-		// 처음페이지, 이전(10페이지 전)
-		n = current_page - numPerBlock;
-		if(total_page > numPerBlock && currentPageSetup > 0) {
+		// 처음페이지
+		if (current_page > 1) {
 			sb.append("<li><a href='" + list_url + "page=1'>&lt;&lt;</a></li>");
+		} else {
+			sb.append("<li><span class='disabled'>&lt;&lt;</span></li>");
+		}
+		
+		// 이전 페이지(5페이지전)
+		n = current_page - numPerBlock;
+		if (total_page > numPerBlock && currentPageSetup > 0) {
 			sb.append("<li><a href='" + list_url + "page=" + n + "'>&lt;</a></li>");
+		} else {
+			sb.append("<li><span class='disabled'>&lt;</span></li>");
 		}
 		
 		// 페이징
@@ -50,12 +58,20 @@ public class MyUtilCustom extends MyUtil {
 			page++;
 		}
 		
-		// 다음(10페이지 후), 마지막페이지
+		// 다음(10페이지 후)
 		n = current_page + numPerBlock;
-		if(n > total_page) n = total_page;
-		if(total_page - currentPageSetup > numPerBlock) {
-			sb.append("<li><a href='" + list_url + "page=" + n + "'>&gt;</a></li>");
-			sb.append("<li><a href='" + list_url + "page=" + total_page + "'>&gt;&gt;</a></li>");
+		if (n > total_page) n = total_page;
+		if (total_page - currentPageSetup > numPerBlock) {
+			sb.append("<li><a href='" + list_url + "page=" + n + ");>&gt;</a></li>");
+		} else {
+			sb.append("<li><span class='disabled'>&gt;</span></li>");
+		}
+		
+		// 마지막 페이지
+		if (current_page < total_page) {
+			sb.append("<li><a href='" + list_url + "page=" + total_page + ");'>&gt;&gt;</a></li>");
+		} else {
+			sb.append("<li><span class='disabled'>&gt;&gt;</span></li>");
 		}
 		
 		sb.append("</ul>");
@@ -90,7 +106,7 @@ public class MyUtilCustom extends MyUtil {
 		} else {
 			sb.append("<li><span class='disabled'>&lt;&lt;</span></li>");
 		}
-		// 이전(10페이지 전)
+		// 이전(5페이지 전)
 		n = current_page - numPerBlock;
 		if (total_page > numPerBlock && currentPageSetUp > 0) {
 			sb.append("<li><a onclick='" + methodName + "(" + n + ");'>&lt;</a></li>");
